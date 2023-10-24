2e2422cc99b4f203e5083afef5dc9f88f95c8f33
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-24 12:34:55 +0900

    10/24

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,4 +1,6 @@
 // 컴포넌트 합성 : 여러 개의 컴포넌트를 조합하여 하나의 컴포넌트를 만드는 것
+import itzyImg from "../src/images/itzy.jpeg";
+
 function Heading(props) {
     return <h1>{props.text}</h1>
 }
@@ -7,7 +9,7 @@ function Paragraph(props) {
 }
 
 function ImgUrl(props) {
-    return <img src={props.url} alt="이미지 테스트"/>
+    return <img style={{height:"300px"}} src={itzyImg} alt="이미지 테스트"/>
 }
 
 const Section = (props) => {

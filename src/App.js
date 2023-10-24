772ddf9ff3f91fee43451378483cc0cf7a7fa55b
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-24 15:12:34 +0900

    10/24

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Fragment } from "react";
-import './App.css';
+import "./App.css";
 import Section from "./Section";
 import MyComponent from "./MyComponent";
-
+import Say from "./Say";
 
 function App() {
-    return (
-      <>
-        {/* <Section title="스포츠" content="This is the content for section 1."/>
+  return (
+    <>
+      {/* <Section title="스포츠" content="This is the content for section 1."/>
         <Section title="정치" content="This is the content for section 2."/> */}
-        <MyComponent name="곰돌이사육사" age={30}/>
-      </>
-    )
-  };
-
+      {/* <MyComponent name="곰돌이사육사" age={30}>
+        안녕하세요^^
+      </MyComponent> */}
+      <Say />
+    </>
+  );
+}
 
 export default App;
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,11 +1,19 @@
 // props 기본값 설정 : defaultProps
-const MyComponent = props => {
-    return <div>안녕하세요. 제 이름은 {props.name} 입니다. 나이는{props.age} 입니다.</div>
-}
+const MyComponent = ({ name, age, children }) => {
+  // 비구조화할당(구조 분해) : props 객체의 속성 혹은 값을 해체하여 그 값을 변수에 각각 담아 사용하는 자바스크립트 표현식
+  //   const { name, age, children } = props;
+
+  return (
+    <div>
+      안녕하세요. 제 이름은 {name} 입니다. 나이는{age}, 인사말
+      {children}
+    </div>
+  );
+};
 
 MyComponent.defaultProps = {
-    name: "기본이름",
-    age: "22"
-}
+  name: "기본이름",
+  age: "22",
+};
 
 export default MyComponent;
